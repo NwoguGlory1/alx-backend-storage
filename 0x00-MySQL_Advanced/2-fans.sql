d6b4a9ec54822549858415da2dc13b9a7f8d726c
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-09 16:24:04 +0000

    Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,8 @@
+-- Import the metal_bands table dump
+-- Assume the table name is metal_bands
+
 -- ranks column name ordered by number of fans
-SELECT origin, nb_fans,
-       RANK() OVER (ORDER BY nb_fans DESC) AS rank
-FROM metal_bands;
+SELECT origin, SUM(nb_fans) AS total_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY total_fans DESC;
