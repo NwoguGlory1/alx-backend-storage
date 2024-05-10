5c1a88cd360fbb292f1315bc20822e7f98e33825
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-10 14:11:27 +0000

    Add Bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,13 +1,19 @@
 --script creates a stored procedure AddBonus that adds a new correction for a student.
 DELIMITER $$
 
-CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(50), IN score INT
+CREATE PROCEDURE AddBonus (
+	IN user_id INT,
+	IN project_name VARCHAR(255),
+	IN score INT
 BEGIN
-	IF NOT EXISTS (SELECT 1 FROM projects WHERE name = project_name) THEN
+	DECLARE project_id INT;
+	IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0 THEN
 		INSERT INTO projects (name) VALUES (project_name);
-	END IF;	
-INSERT INTO corrections VALUES (user_id, project_name, score)
-VALUES (user_id, project_name, score);
+	END IF;
+	SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+	-- sets value of the project_id to id of the project that matches the project_name. 
+	-- LIMIT 1 ensures that only 1 project is considered even if multiple projects have samename
+	INSERT INTO corrections VALUES (user_id, project_id, score) VALUES (user_id, project_id, score);
 
 END$$
 
