059c1731459e4261d551600c1125f5cf45e719b2
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-10 01:34:23 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,6 +4,6 @@
 
 -- Write a SELECT query to retrieve band_name and compute lifespan
 SELECT band_name,
-	(2022 - COALESCE(split, 2022)) - COALESCE(formed, 0) AS lifespan
+	(2022 - formed) - split(lifespan, '-', 1)::INT AS lifespan
 FROM metal_bands WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
