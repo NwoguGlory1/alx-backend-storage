9d22477dbb8814a8b5b818961571b041f2d816b8
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-10 02:00:48 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,5 +3,8 @@
 
 -- Write a SELECT query to retrieve band_name and compute lifespan
 SELECT band_name AS band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
+-- if split column is NULL, it uses 2022 as the formation year
+-- if formed column is NULL, it uses 0 as the value
+-- it calculates longevity by subtracting formed year from split year
 FROM metal_bands WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
