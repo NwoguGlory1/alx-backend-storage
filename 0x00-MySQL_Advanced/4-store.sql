8abf429e1b0b985e087a53720f4bdd58cd72f9e4
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-10 03:06:22 +0000

    Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,5 @@
 
 CREATE TRIGGER after_order_insert
-[AFTER] INSERT ON orders
+AFTER INSERT ON orders
 FOR EACH ROW
-DECLARE current_quantity INT;
-SELECT quantity INTO current_quantity FROM items WHERE name = NEW.item_name;
 UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
