043a0f1955531153944d4cb98716e5d4bed91f7d
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-10 03:00:25 +0000

    Buy buy buy

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE TRIGGER after_order_insert
+[AFTER] INSERT ON orders
+FOR EACH ROW
+	BEGIN
+		DECLARE current_quantity INT;
+		SELECT quantity INTO current_quantity FROM items WHERE name = NEW.item_name;
+		UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
+	END$$
+
+	DELIMITER ;
