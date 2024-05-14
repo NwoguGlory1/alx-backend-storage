71fd80cdf312ab1b8e5440eb7d1f6c2fad3adeba
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-14 06:05:51 -0700

    Insert a document in Python

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,12 +4,13 @@
 from pymongo import MongoClient
 """ import needed to establish a connection to the MongoDB server. """
 
+
 def insert_school(mongo_collection, **kwargs):
     """
     function that inserts a new document in a collection, mongo_collection
     based on kwargs
     """
-    result_of_insertion = mongo_collection.insertOne(kwargs)
+    result_of_insertion = mongo_collection.insert_one(kwargs)
     """
     inserted_id is an attribute of insertOne, contains the _id
     of the newly inserted document."""
