c28cb85021a1ff3b613c97156ba9989e47f1d9b7
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-14 04:48:55 -0700

    List all documents in Python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,10 +5,19 @@ from pymongo import MongoClient
 """ import needed to establish a connection to the MongoDB server. """
 
 def list_all(mongo_collection):
-    """ List all documents in Python """
+    """ function that lists all documents in a collection """
     documents = mongo_collection.find()
+    """
+    calls the find() method on the mongo_collection object
+    find() method retrieves all documents in a collection,
+    it returns a cursor which is stored in documents.
+    """
 
     if documents.count() == 0:
+        """
+        calls the count() method on document,
+        it counts  all documents in a collection.
+        """
         return []
 
     return documents
