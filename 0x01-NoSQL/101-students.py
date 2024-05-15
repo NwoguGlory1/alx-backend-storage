2a01aa4557ee054239031603be6548549743c6ef
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-14 19:11:45 -0700

    Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""script that returns all students sorted by average score """
+
+
+def top_students(mongo_collection):
+    """ function that returns all students sorted by average score """
+    # top_st = mongo_collection.aggregate([
+    #     {"$unwind": "$topics"},
+    #     {"$group":
+    #         {
+    #             "_id": "$_id",
+    #             "name": {"$first": '$name'},
+    #             "averageScore": {"$avg": "$topics.score"}
+    #         }
+    #      },
+    #     {"$sort": {"averageScore": -1}}
+    # ])
+
+    top_st = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg" "$topics.score"}
+                }
+            }
+        {"$sort": {"averageScore": -1}}
+        ])
+    return top_st
