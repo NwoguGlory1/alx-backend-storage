0fe8a3436d8984120b0853e328430730b6cb2fd7
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-18 16:50:50 +0000

    Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" script that writes strings to Redis """
+import redis
+import uuid
+from typing import Union
+""" imports differnt modules"""
+
+
+class Cache:
+    """ declares the class, Cache"""
+    
+    
+    def __init__(self):
+        """Constructor Method """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ store method that stores input data in Redis using a
+        random key, returns the key """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
